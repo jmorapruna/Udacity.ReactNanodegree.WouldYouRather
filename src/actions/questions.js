4f3f8191d7f7b28c33ecd4eb14aee10eee388208
jmorapruna <jmorapruna@gmail.com>
2020-05-24 12:21:13 +0200

    Fixed: question creation does not affect leader board. Moved createQuestion action to shared.

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,32 +1,8 @@
-import { saveQuestion } from '../utils/api'
-
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
-export const CREATE_QUESTION = 'CREATE_QUESTION'
 
 export function receiveQuestions(questions) {
   return {
     type: RECEIVE_QUESTIONS,
     questions
-  }
-}
-
-function createQuestion(question) {
-  return {
-    type: CREATE_QUESTION,
-    question,
-  }
-}
-
-export function handleCreateQuestion(optionOneText, optionTwoText, cb) {
-  return (dispatch, getState) => {
-    const { authedUserId } = getState()
-    
-    return saveQuestion({
-      author: authedUserId,
-      optionOneText,
-      optionTwoText,
-    })
-      .then(question => dispatch(createQuestion(question)))
-      .then(() => cb())
   }
 }
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,5 +1,5 @@
 import { RECEIVE_USERS } from '../actions/users'
-import { SAVE_QUESTION_ANSWER } from '../actions/shared'
+import { SAVE_QUESTION_ANSWER, CREATE_QUESTION } from '../actions/shared'
 
 export default function users(state = {}, action) {
   switch (action.type) {
@@ -17,6 +17,19 @@ export default function users(state = {}, action) {
             ...state[authedUserId].answers,
             [questionId]: answeredOptionId
           }
+        }
+      }
+    }
+
+    case CREATE_QUESTION: {
+      const { question } = action
+      const newQuestionAuthorId = question.author
+      
+      return {
+        ...state,
+        [newQuestionAuthorId]: {
+          ...state[newQuestionAuthorId],
+          questions: [...state[newQuestionAuthorId].questions, question.id]
         }
       }
     }
